7be2d78f564c25c1ca8ef47c6957a6e0c935b452
Kseniia <ks94-box@mail.ru>
2023-01-23 23:09:29 +0600

    added 2 test files

---
+++ test_first.py
@@ -0,0 +1,14 @@
+from selene.support.shared import browser
+from selene import be, have
+import pytest
+
+@pytest.fixture()
+def open_browser():
+    browser.open('https://google.com')
+    print("open the browser")
+
+
+def test_first(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - Wikipedia'))
+    print(open_browser)
